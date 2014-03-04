486c7f5b248e3469ef08c85f965266adf80165bc
jason <xltank@gmail.com>
2014-03-04 18:50:24 +0800

    add parse public/video api result to VideoInfo vo

---
+++ src/com/jasonxuli/test/vo/ThumbnailsVTX.java
@@ -0,0 +1,16 @@
+package com.jasonxuli.test.vo;
+
+public class ThumbnailsVTX {
+
+	public String url = "";
+	public int width = 0;
+	public int height = 0;
+	
+	
+	public ThumbnailsVTX(String u, int w, int h) {
+		url = u;
+		width = w;
+		height = h;
+	}
+	
+}
